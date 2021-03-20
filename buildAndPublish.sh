6728c536586d0d89a51a00887a646f4c8f44f130
Miguel G. Sanguino <sanguino@me.com>
2021-03-20 22:53:17 +0100

    add weatherservice to build script

--- buildAndPublish.sh
+++ buildAndPublish.sh
@@ -9,3 +9,9 @@ DOCKER_HUB_USER=sanguino
 cd $BASEDIR/server
 docker build . -t $DOCKER_HUB_USER/eolo-planner-server
 docker push $DOCKER_HUB_USER/eolo-planner-server
+
+
+cd $BASEDIR/weatherservice
+pack build $DOCKER_HUB_USER/eolo-planner-weatherservice --path . --builder gcr.io/buildpacks/builder:v1
+docker push $DOCKER_HUB_USER/eolo-planner-weatherservice
+
