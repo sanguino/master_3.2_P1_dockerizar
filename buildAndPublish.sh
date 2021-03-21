5eaa3966b28dc564f2a0b8f46d3bf8e32cded4f3
Miguel G. Sanguino <sanguino@me.com>
2021-03-21 10:55:29 +0100

    add toposervice build and publish script

--- buildAndPublish.sh
+++ buildAndPublish.sh
@@ -15,3 +15,9 @@ cd $BASEDIR/weatherservice
 pack build $DOCKER_HUB_USER/eolo-planner-weatherservice --path . --builder gcr.io/buildpacks/builder:v1
 docker push $DOCKER_HUB_USER/eolo-planner-weatherservice
 
+
+cd $BASEDIR/toposervice
+mvn compile jib:build -Dimage=sanguino/eolo-planner-toposervice
+docker push $DOCKER_HUB_USER/eolo-planner-toposervice
+
+
